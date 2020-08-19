1e30ba786a6965fd8df2b643a5d962b06891d4fb
Daniel Sutton <dansut@amazon.com>
2020-08-18 23:12:52 -0700

    Update find_java_libdir.sh to support new directory layout on mac

--- find_java_libdir.sh
+++ find_java_libdir.sh
@@ -47,7 +47,7 @@ main () {
     target_arch=`uname -m`
     if [[ -d ${jre_dir}/${target_arch}/classic ]]; then lib_dir="${jre_dir}"/${target_arch}/classic; else lib_dir="${jre_dir}"/${target_arch}/server; fi
   elif [[ "${os}" == "mac" ]]; then
-    lib_dir="${jre_dir}/jli"
+    if [[ -f ${jre_dir}/jli/libjli.dylib ]]; then lib_dir="${jre_dir}"/jli; else lib_dir="${jre_dir}"; fi
   else
     local arch
     if [[ "${target_arch}" =~ (32|386) ]]; then
